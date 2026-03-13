c74aeed2a45f269c6c60ab8c8ba3229a041d444d
onni <holttaonni@gmail.com>
2026-03-13 08:23:57 +0200

    urjh

--- 3.21-tehtava/index.js
+++ 3.21-tehtava/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const Person = require('./person')
+const Person = require('./backend/person')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
