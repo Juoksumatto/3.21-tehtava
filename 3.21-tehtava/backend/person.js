09dc2d5bb4e1c6046c1e1908adeaeb8a9b4cbd4b
onni <holttaonni@gmail.com>
2026-03-13 08:56:23 +0200

    fdgg

--- 3.21-tehtava/backend/person.js
+++ 3.21-tehtava/backend/person.js
@@ -1,23 +1,5 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
-mongoose.set('strictQuery', false)
-
-process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4})
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
